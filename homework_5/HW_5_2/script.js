a02d25200caf585ec8c4b9c95b09c44922f61732
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-01-15 13:40:27 +0200

    updated script.js

--- homework_5/HW_5_2/script.js
+++ homework_5/HW_5_2/script.js
@@ -1,7 +1,8 @@
+const dollarRate = 26;
 function myFunction() {
     let fragment = new DocumentFragment();
     for (let i = 10; i<= 100; i += 10) {
-        let result = i * 26;
+        let result = i * dollarRate;
         result = `${i} $ = ${result} грн`;
         let el = document.createElement("li"); 
         el.append(result); 
