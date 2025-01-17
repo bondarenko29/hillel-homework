10617b366d59fe12921e1e69a1446d776e41433c
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-01-17 13:45:01 +0200

    updated script.js HW 5.4

--- homework_5/HW_5_4/script.js
+++ homework_5/HW_5_4/script.js
@@ -1,27 +1,25 @@
 function myFunction() {
-    let primeNumber = prompt('Введіть ціле число: ');
+    const primeNumber = prompt('Введіть ціле число: ');
     let divider = 2;
-    let message;
     let result;
     if (primeNumber === null || primeNumber.trim() === '' || isNaN(primeNumber)) {
-        primeNumber = document.getElementById("prime").innerText = `Це не число!
+        document.getElementById("prime").innerText = `Це не число!
         Натисніть ще раз кнопку!
         `; 
     }   
     else if (!Number.isInteger(parseFloat(primeNumber))) {
-        primeNumber = document.getElementById("prime").innerText = `Ваше число ${primeNumber} є десятковим дробом! 
+        document.getElementById("prime").innerText = `Ваше число ${primeNumber} є десятковим дробом! 
         Введіть ціле число.
         Натисніть ще раз кнопку!
         `;
     }
-    else if (parseFloat(primeNumber) <= 1) {
-        primeNumber = document.getElementById("prime").innerText = `Ваше число ${primeNumber}. 
+    else if (primeNumber <= 1) {
+        document.getElementById("prime").innerText = `Ваше число ${primeNumber}. 
         Введіть число > 1. 
         Натисніть ще раз кнопку!
         `;
     }
-    else {
-        primeNumber = parseInt(primeNumber);     
+    else {    
         do {           
             result = primeNumber % divider;
             divider++;                  
@@ -31,6 +29,6 @@ function myFunction() {
             }            
             document.getElementById("prime").innerText = `Ваше число ${primeNumber} - просте!`;
         } 
-        while (divider <= Math.sqrt(primeNumber));      
+        while (divider <= Math.sqrt(primeNumber));             
     }
 } 
