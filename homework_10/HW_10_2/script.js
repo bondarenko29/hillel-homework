d333a6b1b1cdb27e7498b86901d93df084442628
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-02-05 18:46:02 +0200

    added HW_10_2

---
+++ homework_10/HW_10_2/script.js
@@ -0,0 +1,15 @@
+// Перший спосіб
+const arrFirstNumbers = [2, 3, 5, 14, 21, 32, 56, 87, 98, 189678, 234.87, 0, -5678];
+let evenNumbers = [];
+for (const item of arrFirstNumbers) {
+    if(item % 2 === 0){
+        evenNumbers.push(item);
+    }
+}
+console.log(evenNumbers);
+
+// Другий спосіб
+const arrSecondNumbers = [1, 2, 3, 4, 88, 35, 56, 88, 102, 104];
+const newEvenNumbers = arrSecondNumbers.filter(number => number % 2 === 0);
+console.log(newEvenNumbers);
+
