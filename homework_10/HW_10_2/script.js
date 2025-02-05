8e5811331b61166d68328eb0264db3eaf06e8593
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-02-05 19:05:29 +0200

    updated script.js

--- homework_10/HW_10_2/script.js
+++ homework_10/HW_10_2/script.js
@@ -7,9 +7,11 @@ for (const item of arrFirstNumbers) {
     }
 }
 console.log(evenNumbers);
+document.getElementById('array1').textContent = `Новий масив: [${evenNumbers}]`;
 
 // Другий спосіб
 const arrSecondNumbers = [1, 2, 3, 4, 88, 35, 56, 88, 102, 104];
 const newEvenNumbers = arrSecondNumbers.filter(number => number % 2 === 0);
 console.log(newEvenNumbers);
 
+document.getElementById('array2').textContent = `Новий масив: [${newEvenNumbers}]`;
