acef5223028df08afc220c74c85e219253da59ba
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-02-06 17:31:23 +0200

    updated script.js of HW_10_1

--- homework_10/HW_10_1/script.js
+++ homework_10/HW_10_1/script.js
@@ -3,7 +3,6 @@ const user = {
     age: 28,
     location: 'Україна',
     getInfo: function () { 
-      //  const { firstName, age, location } = user;
         console.log(`Вас звати ${this.firstName}. Вам ${this.age} років. Ваше місце проживання - ${this.location}.`);
         document.getElementById('info').textContent = `Вас звати ${this.firstName}. Вам ${this.age} років. Ваше місце проживання - ${this.location}.`;
      }
