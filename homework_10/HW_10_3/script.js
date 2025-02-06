0a7afbcb01ec320ef66eefcc97a6a64389fb3429
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-02-07 01:47:05 +0200

    updated script

--- homework_10/HW_10_3/script.js
+++ homework_10/HW_10_3/script.js
@@ -3,29 +3,29 @@ let usersBook = {
         {name: 'Alex', phone: '+38(091)-111-22-33', email: 'alex@gmail.com'},
         {name: 'Max', phone: '+38(091)-123-26-30', email: 'max@gmail.com'},
     ],
-    addUsers: (name, phone, email) => {
-        const newUser = { name, phone, email };
-        usersBook.users.push(newUser);
+    addUsers(name, phone, email)  {
+        this.users.push({ name, phone, email });
     },
-    findContact: (name) => {
-        const findUser = usersBook.users.filter(user => user.name === name);
+    findContact(name) {
+        const findUser = this.users.filter(user => user.name === name);
         if (findUser.length > 0) {
-            for (let i = 0; i < findUser.length; i++) {
-                console.log(`Користувач ${name} є в телефонній книзі ${JSON.stringify(findUser[i])}`);
-            }
+            console.log(`Користувач ${name} є в телефонній книзі:`);
+            findUser.forEach(user => {
+                console.log(`${JSON.stringify(user)}`);
+            })
         } else {
             console.log(`Користувача ${name} не знайдено в телефонній книзі`);
-            return null;
         }       
     }
 };
 
-const {users, addUsers, findContact} = usersBook;
-addUsers('Denis', '+38(099)-333-33-22', 'den@gmail.com');
-addUsers('Peter', '+38(091)-661-38-99','peter@gmail.com');
-addUsers('Alex', '+38(091)-551-77-55', 'alexandr@gmail.com');
 
-console.log(users);
-findContact('Iryna')
-findContact('Alex');
+usersBook.addUsers('Denis', '+38(099)-333-33-22', 'den@gmail.com');
+usersBook.addUsers('Peter', '+38(091)-661-38-99','peter@gmail.com');
+usersBook.addUsers('Alex', '+38(091)-551-77-55', 'alexandr@gmail.com');
+
+console.log(`В телефонній книзі є ${usersBook.users.length} користувачів: `)
+console.log(usersBook.users);
+usersBook.findContact('Iryna');
+usersBook.findContact('Alex');
 
