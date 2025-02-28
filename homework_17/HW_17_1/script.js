f886e4de7ca90e6c0feca53aabe8421fbf4928ec
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-03-01 01:24:03 +0200

    added HW_17_1

---
+++ homework_17/HW_17_1/script.js
@@ -0,0 +1,61 @@
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return (b !== 0) ?  a / b : 'Помилка: діленняна нуль!';
+    }
+    mod(a, b) {
+        return a % b;
+    }
+    factorial(a) {
+        if (a === 0 || a === 1) {
+            return 1;
+        } else {
+            return a * this.factorial(a - 1);
+        }
+    }
+    abc(a) {
+        return a >= 0 ? a : -a;
+    }
+    sqrt(a) {
+        return Math.sqrt(a);
+    }
+
+    power(a, b) {
+        return Math.pow(a, b);
+    }
+}
+
+const calc = new Calculator();
+
+console.log(calc.add(-28, -56)); // -84
+
+console.log(calc.subtract(57, 99)); // -42
+
+console.log(calc.multiply(39, 3)); // 117
+
+console.log(calc.divide(-68, 4)); // -17
+
+console.log(calc.mod(11, 5)); // 1
+
+console.log(calc.factorial(5)); // 120
+
+console.log(calc.factorial(0)); // 1
+
+console.log(calc.abc(67)); // 67
+console.log(calc.abc(-7)); // 7
+console.log(calc.abc(0)); // 0
+
+console.log(calc.sqrt(16)); // 4
+
+console.log(calc.power(3, -4)); // 81
+
+
+
