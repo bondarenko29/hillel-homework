7e6c5044eb0816d8a65a491e9406dcabfbd9fa07
Iryna Bondarenko <fm.ub15b.bondarenko@gmail.com>
2025-03-07 23:35:04 +0200

    updated script.js

--- homework_18/script.js
+++ homework_18/script.js
@@ -1,14 +1,13 @@
 const timerDisplay = document.getElementById('timer');
-let deadline = new Date('March 08, 2025 19:15:00').getTime();
+let deadline = new Date('March 07, 2025 19:15:00').getTime();
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
 let timerInterval = setInterval(() => {
     let toDay = new Date().getTime();
     let differenceTime = deadline - toDay;
-
-    const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
 
     let days = Math.floor(differenceTime / (day));
     let hours = Math.floor((differenceTime % (day)) / (hour));
@@ -16,10 +15,14 @@ let timerInterval = setInterval(() => {
     let seconds = Math.floor((differenceTime % (minute)) / second);
     timerDisplay.textContent = `${days} days ${String(hours).padStart(2, '0')} : ${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}`;
 
+    if (toDay >= deadline) {
+        timerDisplay.textContent = 'Oops! Time is up! 🤷';
+        return;
+    }
+
     if (differenceTime < 0) {
         clearInterval(timerInterval);
         timerDisplay.textContent = 'TIME RAN OUT';
-
     }
 }, 1000);
 
