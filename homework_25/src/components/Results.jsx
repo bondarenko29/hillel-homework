a4724272da99e208d11d877e8e7e75e6dc05a5eb
IrynaBond <fm.ub15b.bondarenko@gmail.com>
2025-04-08 01:20:51 +0300

    added HW_25

---
+++ homework_25/src/components/Results.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Results =({ showResults, winner, votes }) => {
+    if (!showResults || !winner) {
+        return null;
+    }
+
+    return (
+        <div className="mt-6 p-4 border border-gray-300 rounded shadow-sm text-center">
+            <h2 className="text-xl font-semibold mb-2">Результати голосування:</h2>
+            <p className="mb-1">Переможець:</p>
+            <div className="flex justify-center">
+                <span className="text-5xl">{winner.emoji}</span>
+            </div>
+            <p>
+                Кількість голосів: <span className="font-bold">{votes[winner.id]}</span>
+            </p>
+        </div>
+    )
+
+}
+export default Results;
